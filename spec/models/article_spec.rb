ef4ad92687f014694e3c00e6cb41017a780c4db1
David Stosik <david.stosik+git-noreply@gmail.com>
2019-03-20 15:27:29 +0000

    basic specs to confirm transactional fixtures

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "supports transactional fixtures (1)" do
+    Article.create(title: "supports transactional fixtures (1)")
+
+    expect(Article.count).to eq 1
+  end
+
+  it "supports transactional fixtures (2)" do
+    Article.create(title: "supports transactional fixtures (2)")
+
+    expect(Article.count).to eq 1
+  end
 end
